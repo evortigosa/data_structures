8778166dac6996645a56bee2305ccd0d000640e3
Evandro S. Ortigossa <evortigosa@gmail.com>
2026-06-01 09:48:55 +0300

    Create Pilha

---
+++ Pilha/MyStack.h
@@ -0,0 +1,23 @@
+/*        Aula Prática 4 - POO -
+Evandro Scudeleti Ortigossa, nUSP 6793135 */
+
+#ifndef MYSTACK_H_INCLUDED
+#define MYSTACK_H_INCLUDED
+
+class MyStack {
+    public:
+        MyStack(int);
+        ~MyStack();
+        unsigned int getSize() const;
+        void print() const;
+        void push(int elem, bool *error);
+        int pop(bool *error);
+        void invert();
+
+    private:
+        int maxsize;
+        int top;
+        int *stack;
+};
+
+#endif // MYSTACK_H_INCLUDED
