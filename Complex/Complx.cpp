a1e8a7a6e5fc3b453e4c89a7ccf65a470ab4dab8
Evandro S. Ortigossa <evortigosa@gmail.com>
2026-06-01 09:46:51 +0300

    Add files via upload

---
+++ Complex/Complx.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Complx.h"
+
+using namespace std;
+
+Complx::Complx(double real, double imag) {
+    this->real= real;
+    this->imag= imag;
+}
+
+Complx Complx::operator+(const Complx &c) const {
+    Complx resultado;
+
+    resultado.real= (this->real + c.real);
+    resultado.imag= (this->imag + c.imag);
+
+    return resultado;
+}
+
+void Complx::print() {
+    cout << real << " + " << imag << "*i" << endl;
+}
---
+++ Complex/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Complx.h"
+
+using namespace std;
+
+int main() {
+    Complx x(3, 4);
+    Complx y(6, 7);
+
+    Complx z= x + y;
+    z.print();
+
+    return 0;
+
+}
